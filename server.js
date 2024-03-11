a032b982b7713382806d878497e3a58eda59cc5e
MonikaGellert <113904942+MonikaGellert@users.noreply.github.com>
2024-03-11 19:42:36 +0100

    add mutter do server and package json

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-import app from "./app.js";
+import express from 'express';
+import multer from 'multer';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); 
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  
+  res.send('Plik przesłany pomyślnie!');
+});
+
 const dbConnectionURI = process.env.DB_URL;
 
 mongoose.connect(dbConnectionURI, {
@@ -12,14 +34,14 @@ mongoose.connect(dbConnectionURI, {
 
 const db = mongoose.connection;
 
-db.on("error", (error) => {
-  console.error("Database connection error:", error);
+db.on('error', (error) => {
+  console.error('Database connection error:', error);
   process.exit(1);
 });
 
-db.once("open", () => {
-  console.log("Database connection successful");
-  app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000");
+db.once('open', () => {
+  console.log('Database connection successful');
+  app.listen(port, () => {
+    console.log(`Server running. Use our API on port: ${port}`);
   });
 });
