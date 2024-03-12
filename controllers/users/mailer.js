039ed0825fa13b43dadc77fdd2fa5184126edafa
MonikaGellert <113904942+MonikaGellert@users.noreply.github.com>
2024-03-12 09:50:13 +0100

    add sendgrid

--- controllers/users/mailer.js
+++ controllers/users/mailer.js
@@ -27,5 +27,23 @@ const sendVerificationEmail = (recipientEmail, verificationToken) => {
     }
   });
 };
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'moniamber@gmail.com.com', 
+  from: 'monika.gellert@wp.pl',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  
 
 export default sendVerificationEmail;
+  }
